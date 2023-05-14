4289761be75767b38aef98af95f17d936d2fe927
Brian Lindner <brian@bclindner.com>
2023-05-14 11:51:33 -0400

    removed log traces

--- lib/cnmn/command/music/consumer.ex
+++ lib/cnmn/command/music/consumer.ex
@@ -5,10 +5,8 @@ defmodule CNMN.Command.Music.Consumer do
   use Nostrum.Consumer
   alias Nostrum.Voice
   alias CNMN.Command.Music.Agent
-  require Logger
 
   def run_player(guild_id) do
-    Logger.info("player running for #{guild_id}")
     # get the first item from the queue and start playing it
     case Agent.pop(guild_id) do
       nil ->
@@ -22,8 +20,6 @@ defmodule CNMN.Command.Music.Consumer do
   # if there is a speaking update and the bot is no longer speaking, then we
   # can re-run the player
   def handle_event({:VOICE_SPEAKING_UPDATE, evt, _ws_state}) do
-    Logger.info("speaking update")
-
     if !evt.speaking && !evt.timed_out do
       run_player(evt.guild_id)
     end
