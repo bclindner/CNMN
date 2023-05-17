3dee27444a40a30287e1cf23252ac75b291ca07e
Brian Lindner <brian@bclindner.com>
2023-05-16 22:33:17 -0400

    from_youtube_dl!/1: added url scheme check

--- lib/cnmn/command/music/track.ex
+++ lib/cnmn/command/music/track.ex
@@ -14,7 +14,13 @@ defmodule CNMN.Command.Music.Track do
   Parse a YoutubeDL-compatible audio format into a `CNMN.Command.Music.Track`.
   """
   def from_youtube_dl!(url) do
-    # call youtube-dl directly to get what we want
+    # quick verification - let's make sure this is https. we do this primarily
+    # so that people can't abuse ytsearch and other plugins that will break our
+    # embeds and other discord functions that require us to input URLS
+    unless String.starts_with?(url, "https://") do
+      raise "link is invalid"
+    end
+    # now call youtube-dl directly to get what we want
     {output, status} = System.cmd(ytdl_path(), ["-s", "-j", url])
 
     case status do
