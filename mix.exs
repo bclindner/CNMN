11f5bf4e546f718a8e233ca764a02c169ef1c553
Brian C. Lindner <brian@bclindner.com>
2023-07-24 21:14:12 -0400

    1.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CNMN.MixProject do
   def project do
     [
       app: :cnmn,
-      version: "1.2.1",
+      version: "1.2.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
