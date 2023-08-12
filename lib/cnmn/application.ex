f95ff3c4c14970334c65f6f68c0ffac21351895f
Brian C. Lindner <brian@bclindner.com>
2023-08-12 13:22:45 -0400

    fixed mix env (forgot you weren't supposed to do that)

--- lib/cnmn/application.ex
+++ lib/cnmn/application.ex
@@ -12,10 +12,12 @@ defmodule CNMN.Application do
         # music state manager
         {CNMN.Music.Manager, %{}}
       ] ++
-        if Mix.env() != :test,
-          # event consumer
-          do: [CNMN.Consumer],
-          else: []
+        # add the consumer only if test mode is disabled
+        # (this prevents the system from launching nostrum and logging in)
+        case Application.fetch_env(:cnmn, :test_mode) do
+          {:ok, :true} -> []
+          _ -> [CNMN.Consumer]
+        end
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
