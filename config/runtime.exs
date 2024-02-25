1607dfd57907b843cd4dad0f36b7c3b2b6ec5fc7
Brian Lindner <brian@bclindner.com>
2024-02-25 12:17:26 -0500

    fixed autotransform

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,6 +7,6 @@ config :cnmn, :router, prefix: System.get_env("BOT_PREFIX", "c-")
 
 config :cnmn,
   autotransform: %{
-    1_107_403_690_630_987_846 => &CNMN.Image.crunch/2,
-    994_748_788_520_521_778 => &CNMN.Image.crunch/2
+    1_107_403_690_630_987_846 => CNMN.Image.crunch(),
+    994_748_788_520_521_778 => CNMN.Image.crunch()
   }
