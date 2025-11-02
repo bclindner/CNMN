9663696829648cc933fafdcf2a7fd32847218d15
Brian Lindner <brian@bclindner.com>
2025-11-02 18:14:05 -0500

    1.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CNMN.MixProject do
   def project do
     [
       app: :cnmn,
-      version: "1.3.1",
+      version: "1.4.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
